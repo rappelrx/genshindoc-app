d09cc292a429904f93a6b48036f2dd5747e445e2
Rappel R <rappelricafort@gmail.com>
2025-03-19 00:48:40 -0700

    Attribute binding

---
+++ pyro.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref } from 'vue'
+  
+  const visionClass = ref('vision')
+</script>
+
+
+<template>
+  <h1 v-bind:class="visionClass">Pyro represents fire</h1>
+</template>
+
+
+<style>
+  .vision {
+    color: red;
+    animation: rotation 1.5s infinite linear;
+  }
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(-12deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+</style>
