838b8f2ce59a46e90ca9b473f6997f82e2d3e13a
Rappel R <rappelricafort@gmail.com>
2025-03-19 00:31:00 -0700

    Declarative rendering

---
+++ yanfei.vue
@@ -0,0 +1,26 @@
+<script setup>
+	import { reactive, ref } from 'vue'
+
+  const yanfei = reactive({
+    age: 17,
+    vision: 'none',
+    residence: 'Fontaine',
+    job: 'legal intern'
+  })
+  const dialogue = ref('Not ready for trial.')
+  
+  // After one year...
+  yanfei.age++
+  yanfei.vision = 'pyro'
+  yanfei.residence = 'Liyue'
+  yanfei.job = 'lawyer'
+  dialogue.value = 'Ready for trial!'
+</script>
+
+
+<template>
+  <h1>Yanfei says: {{dialogue}}</h1>
+  <p>
+    Yanfei is an {{yanfei.age}} year old {{yanfei.job}} living in {{yanfei.residence}}.
+  </p>
+</template>
