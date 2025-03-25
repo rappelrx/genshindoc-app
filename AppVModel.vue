a0e1a7ee79aeb030669cb8f777d3b75109aa52d6
Rappel R <rappelricafort@gmail.com>
2025-03-25 02:35:41 -0700

    Import components that use v-model

---
+++ AppVModel.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import QuestsList from './QuestsToDoWithFilter.vue'
+  import CharacterSearch from './WatchGenshinChar.vue'
+</script>
+
+<template>
+  <h2>Quests</h2>
+  <QuestsList />
+  <h2>Character Search</h2>
+  <CharacterSearch />
+</template>
+
+<style>
+  html {
+    padding: 0 30px 20px;
+  }
+  h2 {
+    margin-top: 30px;
+  }
+</style>
